99105cdcb85123c1a642c1b8e6a60252b5c3c30a
sindhum25 <84011633+sindhum25@users.noreply.github.com>
2021-07-27 09:08:03 +0530

    change

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello Testing DevOp welcome to devOps Project')
+  response.send('Hello  welcome to devOps sample project')
 }) 
 
 app.listen(app.get('port'), function() {
